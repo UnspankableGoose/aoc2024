ca7ecd039c69b737e4269b704fc42fb3b49332d1
Goose <41350325+UnspankableGoose@users.noreply.github.com>
2024-12-03 00:02:04 -0600

    Add day 1 submission
    this one is in c++ just because

---
+++ aoc1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	vector<int> list1(1000);
+	vector<int> list2(1000);
+
+	for (int i = 0; i < 1000; i++) cin >> list1[i] >> list2[i];
+
+	sort(list1.begin(), list1.end());
+	sort(list2.begin(), list2.end());
+	long long X = 0;
+	for (int i = 0; i < 1000; i++) {
+		X += abs(list2[i] - list1[i]);
+	}
+	cout << X << endl;
+
+	X = 0;
+	for (int i = 0; i < 1000; i++) {
+		int count = 0;
+		for (int j = 0; j < 1000; j++) {
+			if (list1[i] == list2[j]) count++;
+		}
+		X += list1[i] * count;
+	}
+
+	cout << X << endl;
+}
